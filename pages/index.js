8e7b4dbf9201c59ecba15d67447322bb69c3e306
masato932 <masatowada66@gmail.com>
2021-04-25 14:41:04 +0900

    commit

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home({ posts }) {
   return (
     <div>
       <Head>
-        <title>NextJS Startup</title>
+        <title>NextJS Startup Project</title>
       </Head>
       <div className="mb-10 p-3 border rounded">
         <Image
@@ -20,7 +20,7 @@ export default function Home({ posts }) {
         />
       </div>
       <div className="flex justify-center flex-col items-center mb-10">
-        <div className="text-lg mb-3">BLOG POSTS</div>
+        <div className="text-lg mb-3 text-blue-300">BLOG POSTS</div>
         <div className="border w-14"></div>
       </div>
       <div className="flex flex-wrap -m-4 mb-5">
